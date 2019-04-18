83259518ed0430be12f16469bd4e842f3a04ea05
lh <1466947051@qq.com>
2019-04-18 19:27:39 +0800

    生成迁移文件，执行迁移

--- demo01/votetest/models.py
+++ demo01/votetest/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    qname = models.CharField(max_length=50)
+    qpoll = models.IntegerField(default=0)
+
+
+class Choice(models.Model):
+    cname = models.CharField(max_length=20)
+    cpoll = models.IntegerField(default=0)
+    cque = models.ForeignKey('Question', on_delete=models.CASCADE)
+
