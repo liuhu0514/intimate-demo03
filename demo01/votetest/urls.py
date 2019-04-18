a6b9c745acfcf0e57d39806168b06ae2904f1f28
lh <1466947051@qq.com>
2019-04-18 21:40:21 +0800

    解除硬编码

--- demo01/votetest/urls.py
+++ demo01/votetest/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'votetest'
+
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^vote/(\d+)/$', views.vote),
-    url(r'^addvote/(\d)/$', views.addvote),
-    url(r'^detail/(\d+)/$', views.detail),
+    url(r'^$', views.index, name='index'),
+    url(r'^vote/(\d+)/$', views.vote, name='vote'),
+    url(r'^addvote/(\d)/$', views.addvote, name='addvote'),
+    url(r'^detail/(\d+)/$', views.detail, name='detail'),
 ]
