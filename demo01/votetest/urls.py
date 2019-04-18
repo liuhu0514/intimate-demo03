cf65e09232d2ac15457011778f9d135f432d4bdc
lh <1466947051@qq.com>
2019-04-18 21:30:02 +0800

    硬编码版本

--- demo01/votetest/urls.py
+++ demo01/votetest/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^vote/$', views.vote),
-    url(r'^detail/$', views.detail),
+    url(r'^vote/(\d+)/$', views.vote),
+    url(r'^addvote/(\d)/$', views.addvote),
+    url(r'^detail/(\d+)/$', views.detail),
 ]
--- demo01/votetest/views.py
+++ demo01/votetest/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from .models import Question, Choice
 
 # Create your views here.
 
 
 def index(request):
-    temp = loader.get_template('votetest/index.html')
-    temp = temp.render()
-    return HttpResponse(temp)
+    qs = Question.objects.all()
+    return render(request, 'votetest/index.html', {'qs': qs})
 
 
-def vote(request):
-    temp = loader.get_template('votetest/vote.html')
-    temp = temp.render()
-    return HttpResponse(temp)
+def vote(request, qid):
+    q = Question.objects.get(pk=qid)
+    # cs = q.choice_set.all()
+    return render(request, 'votetest/vote.html', {'q': q})
 
-def detail(request):
 
-    return render(request, 'votetest/detail.html')
+def addvote(request, qid):
+    cid = request.POST['option']
+    c = Choice.objects.get(pk=cid)
+    c.cpoll += 1
+    c.save()
+    q = Question.objects.get(pk=qid)
+    q.qpoll += 1
+    q.save()
+    return HttpResponseRedirect('/votetest/detail/'+str(qid)+'/', {'qid': qid})
+
+
+def detail(request, qid):
+    q = Question.objects.get(pk=qid)
+
+    return render(request, 'votetest/detail.html', {'q': q})
 
