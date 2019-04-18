2c4b89393d56d0de90c868ed0a6e1127936cb442
lh <1466947051@qq.com>
2019-04-18 19:57:47 +0800

    后台管理注册完成

--- demo01/votetest/admin.py
+++ demo01/votetest/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'poll', 'que']
+    list_filter = ['cname']
+    search_fields = ['cname']
+    list_per_page = 5
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # 后台管理显示字段
+    list_display = ['id', 'name', 'poll']
+    # 过滤字段
+    list_filter = ['qname']
+    # 搜索字段
+    search_fields = ['qname']
+    # 分页数量
+    list_per_page = 5
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- demo01/votetest/models.py
+++ demo01/votetest/models.py
@@ -7,9 +7,35 @@ class Question(models.Model):
     qname = models.CharField(max_length=50)
     qpoll = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.qname
+
+    def name(self):
+        return self.qname
+    name.short_description = '问题'
+
+    def poll(self):
+        return self.qpoll
+    poll.short_description = '参与人次'
+
 
 class Choice(models.Model):
     cname = models.CharField(max_length=20)
     cpoll = models.IntegerField(default=0)
     cque = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.cname
+
+    def name(self):
+        return self.cname
+    name.short_description = '问题选项'
+
+    def poll(self):
+        return self.cpoll
+    poll.short_description = '票数'
+
+    def que(self):
+        return self.cque
+    que.short_description = '问题'
 
